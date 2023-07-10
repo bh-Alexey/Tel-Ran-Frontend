ff27f8f8b087d4b6c5496acb4e4b71f9318b430e
Oleksiy Kovalchuk <105103739+bh-Alexey@users.noreply.github.com>
2023-07-09 22:07:58 -0400

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,7 +1,10 @@
 let number = +prompt("Enter the number");
 let result = number % 10;
 if ((number > 0) && (number < 10)) {
-    alert(number)
+    alert(number);
+}
+else if ((number > 0) && (number > 10)) {
+    alert(result);
 }
 else if ((number < 0) && (number > -10 )){
     alert(-number);
